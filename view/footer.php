a15a9ea8856a1c02feaeb6211a2526a49132aa37
cuong1911065503 <cv334796@gmai.com>
2023-02-27 08:47:00 +0700

    Su dung slideshow vao Trang Chu

--- view/footer.php
+++ view/footer.php
@@ -1,3 +1,19 @@
+<div class="row mb demo header"><h1>2023</h1></div> 
+<script>
+    let slideIndex = 0;
+    showSlides();
 
+    function showSlides() {
+        let i;
+        let slides = document.getElementsByClassName("mySlides");
+        for (i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
+        }
+        slideIndex++;
+        if (slideIndex > slides.length) {slideIndex = 1}
+        slides[slideIndex-1].style.display = "block";
+        setTimeout(showSlides, 4000); // thời gian ảnh chạy sau 4 giây
+}
+</script>
 </body>
 </html>
--- view/home.php
+++ view/home.php
@@ -1,8 +1,36 @@
 <div class="row mb ">
             <div class="boxtrai mr">
                 <div class="row ">
-                    <div class="banner ">
-                        <img src="images/wlop-16se.jpg" alt="">
+                    <!-- Slideshow container -->
+                    <div class="slideshow-container">
+
+                    <!-- Full-width images with number and caption text -->
+                    <div class="mySlides fade">
+                    <div class="numbertext">1 / 3</div>
+                    <img src="images/1.jpg" style="width:100%">
+                    </div>
+
+                    <div class="mySlides fade">
+                    <div class="numbertext">2 / 3</div>
+                    <img src="images/2.jpg" style="width:100%">
+                    </div>
+
+                    <div class="mySlides fade">
+                    <div class="numbertext">3 / 3</div>
+                    <img src="images/3.jpg" style="width:100%">
+                    </div>
+
+                    <!-- Next and previous buttons -->
+                    <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                    <a class="next" onclick="plusSlides(1)">&#10095;</a>
+                    </div>
+                    <br>
+
+                    <!-- The dots/circles -->
+                    <div style="text-align:center">
+                    <span class="dot" onclick="currentSlide(1)"></span>
+                    <span class="dot" onclick="currentSlide(2)"></span>
+                    <span class="dot" onclick="currentSlide(3)"></span>
                     </div>
                 </div>
                 <div class="row ">
@@ -74,7 +102,6 @@
                     </div>
                 </div>
             </div>
-        </div>
-        <div class="row mb demo header"><h1>2023</h1></div>  
-        <div class="row">
+        </div> 
+        
     
